b0b9faa2639efbefcba5d433790ed34656feb5dc
Shivam <shivamgoswami2357@gmail.com>
2024-09-17 19:24:02 +0530

    footer section responsive

---
+++ resources/views/layout/footer.blade.php
@@ -0,0 +1,36 @@
+<footer class="py-14 lg:py-28 bg-black/85">
+    <div class="lg:max-w-6xl mx-auto px-2.5 sm:px-20 md:px-10"> 
+        <div class="flex flex-col md:flex-row items-center justify-between gap-4">
+            <div class="text-center md:text-left pb-4 lg:pb-0">
+                <h4 class="text-white text-xl md:text-2xl lg:text-3xl pb-2">Let's make something great together</h4>
+                <p class="text-gray-500">Get in touch with us and send some basic info for a quick quote</p>
+            </div>
+            <a href="#" class="border-2 px-8 py-3 text-white hover:bg-white hover:text-black uppercase font-montserrat text-xs font-semibold tracking-wider transition duration-300 ease-in-out">Start a project</a>
+        </div>
+        <div class="before:block before:bg-gray-500 before:w-full before:h-px before:my-12 lg:before:my-20">
+            <div class="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4 items-center">
+                <div class="flex md:block justify-center  pb-3 md:pb-8  lg:pb-0 md:col-span-3 lg:col-span-1">
+                    <img src="{{ asset('images/logo-white.png') }}" alt="" class="w-28">
+                </div>
+                <div class="text-center flex items-center justify-center md:justify-start md:text-left pb-3 lg:pb-0">
+                    <p class="text-gray-500">301 The Greenhouse,
+                        <span class="flex flex-wrap">Custard Factory, London, E2 8DY.</span>
+                    </p>
+                </div>
+                <div class="flex items-center justify-center lg:justify-end  pb-5 lg:pb-0">
+                    <p class="text-gray-500">+44 (0) 123 456 7890 
+                        <span class="flex flex-wrap"><a href="#" class="hover:text-rose-600">sales@domain.com</a></span>
+                    </p>
+                </div>
+                <div class="">
+                    <ul class="text-white flex justify-center md:justify-end gap-5">
+                        <li><a href="#" class="hover:text-rose-600"><i class="fa-brands fa-facebook-f"></i></a></li>
+                        <li><a href="#" class="hover:text-rose-600"><i class="fa-brands fa-twitter"></i></a></li>
+                        <li><a href="#" class="hover:text-rose-600"><i class="fa-brands fa-google-plus-g"></i></a></li>
+                        <li><a href="#" class="hover:text-rose-600"><i class="fa-brands fa-instagram"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
